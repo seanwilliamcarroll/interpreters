b1d5725e7896ca6a6b1802e51c34270040ed8c74
Jonathon Bell <jonathon.bell@cambridgesemantics.com>
2023-12-23 17:27:17 -0500

    Suggested multi-project setup

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# interpreters
+# Interpreters
+
+### Mac OS Toolchain
+
+- [MacPorts](https://www.macports.org)
+- clang-17
+- cmake
+
+After installing MacPorts:
+
+    > sudo port install clang-7 cmake
+    > sudo port select clang mp-clang-17
+
+### Building
+
+    cd <project-dir>
+    > makedir .bld
+    > cd .bld
+    > cmake ..
+    > make -j
+
+### Running
+
+    > make && blip/blip
+
+### Running Tests
+
+    > make && core/core-test
---
+++ core/src/example.cpp
@@ -0,0 +1,32 @@
+//********** Copyright © 2023 Sean Caroll, Jonathon Bell. All rights reserved.
+//*
+//*
+//*  Version : $Header:$
+//*
+//*
+//*  Purpose : An example library source file.
+//*
+//*
+//****************************************************************************
+
+#include "sc/example.hpp"                                // For example()
+
+//****************************************************************************
+namespace sc {
+//****************************************************************************
+
+/**
+ * Returns the given integer multiplied by 3.
+ *
+ * @param  i  The integer to triple.
+ *
+ * @return 'i' multiplied by 3.
+ */
+int example(int i)
+{
+  return i * 3;
+}
+
+//****************************************************************************
+} // namespace sc
+//****************************************************************************
