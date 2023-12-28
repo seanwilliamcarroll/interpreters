650d77fef85d8b77179eba6156626da2b8d6cfc5
Sean Carroll <seanwilliamcarroll@gmail.com>
2023-12-28 11:00:53 -0500

    Add further keyword tokens

--- blip/src/lexer.cpp
+++ blip/src/lexer.cpp
@@ -21,7 +21,13 @@ Lexer::Lexer(std::istream &in_stream, const std::string &file_name)
     : CoreLexer(in_stream, construct_keywords_map(), file_name) {}
 
 KeywordsMap Lexer::construct_keywords_map() {
-  KeywordsMap keywords = {{"if", blip_token_types::IF}};
+  KeywordsMap keywords = {
+      {"if", blip_token_types::IF},   {"while", blip_token_types::WHILE},
+      {"set", blip_token_types::SET}, {"begin", blip_token_types::BEGIN},
+      {"+", blip_token_types::PLUS},  {"-", blip_token_types::MINUS},
+      {"*", blip_token_types::TIMES}, {"/", blip_token_types::DIV},
+      {"=", blip_token_types::EQ},    {"<", blip_token_types::LT},
+      {">", blip_token_types::GT},    {"print", blip_token_types::PRINT}};
   return keywords;
 }
 
