f93979ddee6cd1db8a15ca2e39ba3175659153ec
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-03-31 09:24:22 -0400

    Basic evaluator for various statements, not functions yet

--- blip/src/evaluator.cpp
+++ blip/src/evaluator.cpp
@@ -9,8 +9,11 @@
 //*
 //****************************************************************************
 
+#include "value.hpp"
 #include <evaluator.hpp>
 #include <exceptions.hpp>
+#include <stdexcept>
+#include <variant>
 
 //****************************************************************************
 namespace blip {
@@ -28,39 +31,33 @@ Value Evaluator::evaluate(const AstNode &node) {
 
 // --- Literals --------------------------------------------------------------
 
-void Evaluator::visit(const IntLiteral &node) {
-  // TODO: store node's value in m_result
-  (void)node;
-}
+void Evaluator::visit(const IntLiteral &node) { m_result = node.get_value(); }
 
 void Evaluator::visit(const DoubleLiteral &node) {
-  // TODO: store node's value in m_result
-  (void)node;
+  m_result = node.get_value();
 }
 
 void Evaluator::visit(const StringLiteral &node) {
-  // TODO: store node's value in m_result
-  (void)node;
+  m_result = node.get_value();
 }
 
-void Evaluator::visit(const BoolLiteral &node) {
-  // TODO: store node's value in m_result
-  (void)node;
-}
+void Evaluator::visit(const BoolLiteral &node) { m_result = node.get_value(); }
 
 void Evaluator::visit(const Identifier &node) {
-  // TODO: look up node's name in m_env, store in m_result
-  // Catch std::runtime_error from Environment and re-throw
-  // as CompilerException with node.get_location()
-  (void)node;
+  try {
+    m_result = m_env->lookup(node.get_name());
+  } catch (std::runtime_error &error) {
+    throw core::CompilerException("RuntimeError", error.what(),
+                                  node.get_location());
+  }
 }
 
 // --- Structure -------------------------------------------------------------
 
 void Evaluator::visit(const ProgramNode &node) {
-  // TODO: evaluate each child expression in sequence
-  // m_result is the value of the last expression (or Unit if empty)
-  (void)node;
+  for (const auto &expression : node.get_program()) {
+    expression->accept(*this);
+  }
 }
 
 void Evaluator::visit(const CallNode &node) {
@@ -72,39 +69,76 @@ void Evaluator::visit(const CallNode &node) {
 // --- Special forms ---------------------------------------------------------
 
 void Evaluator::visit(const IfNode &node) {
-  // TODO: evaluate condition, check truthiness,
-  // evaluate then or else branch accordingly
-  (void)node;
+  node.get_condition().accept(*this);
+  if (!std::holds_alternative<bool>(m_result)) {
+    throw core::CompilerException("RuntimeError",
+                                  "Expect bool type in IF condition, not:" +
+                                      value_to_string(m_result),
+                                  node.get_location());
+  }
+  auto condition = std::get<bool>(m_result);
+
+  if (condition) {
+    node.get_then_branch().accept(*this);
+  } else if (node.get_else_branch() != nullptr) {
+    node.get_else_branch()->accept(*this);
+  } else {
+    m_result = Unit{};
+  }
 }
 
 void Evaluator::visit(const WhileNode &node) {
   // TODO: loop while condition is truthy, evaluate body each iteration
   // result is Unit
-  (void)node;
+  while (true) {
+    node.get_condition().accept(*this);
+    if (!std::holds_alternative<bool>(m_result)) {
+      throw core::CompilerException(
+          "RuntimeError",
+          "Expect bool type in WHILE condition, not:" +
+              value_to_string(m_result),
+          node.get_location());
+    }
+    if (!std::get<bool>(m_result)) {
+      break;
+    }
+    node.get_body().accept(*this);
+  }
+  m_result = Unit{};
 }
 
 void Evaluator::visit(const SetNode &node) {
-  // TODO: evaluate value expression, call m_env->set()
-  // result is Unit
-  (void)node;
+  node.get_value().accept(*this);
+  try {
+    m_env->set(node.get_name().get_name(), m_result);
+  } catch (std::runtime_error &error) {
+    throw core::CompilerException("RuntimeError", error.what(),
+                                  node.get_location());
+  }
+  m_result = Unit{};
 }
 
 void Evaluator::visit(const BeginNode &node) {
-  // TODO: evaluate each expression in sequence
-  // result is the value of the last expression
-  (void)node;
+  for (const auto &expression : node.get_expressions()) {
+    expression->accept(*this);
+  }
 }
 
 void Evaluator::visit(const PrintNode &node) {
-  // TODO: evaluate expression, write value_to_string() to m_out
-  // result is Unit
-  (void)node;
+  node.get_expression().accept(*this);
+  m_out << value_to_string(m_result) << "\n";
+  m_result = Unit{};
 }
 
 void Evaluator::visit(const DefineVarNode &node) {
-  // TODO: evaluate value expression, call m_env->define()
-  // result is Unit
-  (void)node;
+  node.get_value().accept(*this);
+  try {
+    m_env->define(node.get_name().get_name(), m_result);
+  } catch (std::runtime_error &error) {
+    throw core::CompilerException("RuntimeError", error.what(),
+                                  node.get_location());
+  }
+  m_result = Unit{};
 }
 
 void Evaluator::visit(const DefineFnNode &node) {
