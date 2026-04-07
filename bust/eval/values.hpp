fe70400137edeac49b88f43252a20dbdf4750dcf
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-04-06 21:35:43 -0400

    Basic outline for value types

--- bust/eval/values.hpp
+++ bust/eval/values.hpp
@@ -11,11 +11,45 @@
 #pragma once
 //****************************************************************************
 
+#include "hir/nodes.hpp"
+#include "types.hpp"
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <variant>
+#include <vector>
+
 //****************************************************************************
 namespace bust::eval {
 //****************************************************************************
 
-// TODO: define Value variant (i64, bool, unit, closure/function)
+struct Environment {};
+
+template <PrimitiveType InnerType> struct AbstractValue {
+  const static PrimitiveType m_type = InnerType;
+};
+
+template <> struct AbstractValue<PrimitiveType::BOOL> {
+  const static PrimitiveType m_type = PrimitiveType::BOOL;
+  bool m_value;
+};
+
+template <> struct AbstractValue<PrimitiveType::I64> {
+  const static PrimitiveType m_type = PrimitiveType::I64;
+  int64_t m_value;
+};
+
+using Bool = AbstractValue<PrimitiveType::BOOL>;
+using I64 = AbstractValue<PrimitiveType::I64>;
+using Unit = AbstractValue<PrimitiveType::UNIT>;
+
+struct Closure {
+  std::vector<std::string> m_parameters;
+  const hir::Expression *m_expression;
+  std::shared_ptr<Environment> m_env;
+};
+
+using Value = std::variant<Bool, I64, Unit, Closure>;
 
 //****************************************************************************
 } // namespace bust::eval
