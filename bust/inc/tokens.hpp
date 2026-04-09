440bd18dd3c7d131ae81f93e434011ea6cced256
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-04-09 12:59:53 -0400

    Handle new tokens in the lexer properly

--- bust/inc/tokens.hpp
+++ bust/inc/tokens.hpp
@@ -11,9 +11,12 @@
 #pragma once
 //****************************************************************************
 
+#include <array>
 #include <cstdint>
 #include <iosfwd>
 #include <string>
+#include <string_view>
+#include <utility>
 
 #include <lexer_interface.hpp>
 #include <token.hpp>
@@ -36,6 +39,7 @@ enum class TokenType : uint8_t {
   EQUALS,
   PIPE,
   AND,
+  AS,
 
   // Arithmetic operators
   PLUS,
@@ -60,6 +64,7 @@ enum class TokenType : uint8_t {
   // Literals
   IDENTIFIER,
   INT_LITERAL,
+  CHAR_LITERAL,
 
   // Keywords
   FN,
@@ -73,8 +78,11 @@ enum class TokenType : uint8_t {
   FALSE,
 
   // Type keywords
+  I8,
+  I32,
   I64,
   BOOL,
+  CHAR,
   UNIT,
 };
 
@@ -84,9 +92,29 @@ std::ostream &operator<<(std::ostream &, const TokenType &);
 using Token = core::Token<TokenType>;
 using TokenIdentifier = core::TokenOf<TokenType, std::string>;
 using TokenNumber = core::TokenOf<TokenType, std::string>;
+using TokenChar = core::TokenOf<TokenType, std::string>;
 using LexerInterface = core::LexerInterface<TokenType>;
 
 std::string token_type_to_string(TokenType type);
+
+inline constexpr std::array<std::pair<std::string_view, TokenType>, 15>
+    keywords{{
+        {"fn", TokenType::FN},
+        {"let", TokenType::LET},
+        {"return", TokenType::RETURN},
+        {"if", TokenType::IF},
+        {"else", TokenType::ELSE},
+        {"while", TokenType::WHILE},
+        {"for", TokenType::FOR},
+        {"true", TokenType::TRUE},
+        {"false", TokenType::FALSE},
+        {"as", TokenType::AS},
+        {"i8", TokenType::I8},
+        {"i32", TokenType::I32},
+        {"i64", TokenType::I64},
+        {"bool", TokenType::BOOL},
+        {"char", TokenType::CHAR},
+    }};
 
 //****************************************************************************
 } // namespace bust
