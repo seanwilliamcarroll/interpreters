c91fba5687679cb6168218d9166e66ce01ee4451
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-05-03 14:27:52 -0400

    Also collect Place during free variable collection

--- bust/zir/free_variable_collector.hpp
+++ bust/zir/free_variable_collector.hpp
@@ -121,8 +121,11 @@ struct FreeVariableCollector {
     return free_variables;
   }
 
+  FreeVariables collect(const Place &place) { return std::visit(*this, place); }
+
   FreeVariables operator()(const Assignment &assignment) {
     FreeVariables free_variables;
+    collect_append(assignment.m_place, free_variables);
     collect_append(assignment.m_expression, free_variables);
     return free_variables;
   }
