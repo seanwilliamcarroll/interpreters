96f5be6aded7e326fc007b2e2e028fd76314bed2
Sean Carroll <seanwilliamcarroll@gmail.com>
2026-05-07 14:06:37 -0400

    codegen portions of while and bare break statement

--- bust/codegen/block_label.hpp
+++ bust/codegen/block_label.hpp
@@ -9,7 +9,9 @@
 #pragma once
 //****************************************************************************
 
+#include <cassert>
 #include <string>
+#include <vector>
 
 //****************************************************************************
 namespace bust::codegen {
@@ -30,6 +32,39 @@ private:
   friend struct IRBuilder;
 };
 
+struct BlockLabelStack {
+
+  void push_scope(BlockLabel label) { m_block_labels.push_back(label); }
+  void pop_scope() noexcept {
+    assert(!m_block_labels.empty() &&
+           "Cannot pop scope, already at global scope!");
+    m_block_labels.pop_back();
+  }
+
+  [[nodiscard]]
+  const BlockLabel &current_block_label() const {
+    return m_block_labels.back();
+  }
+
+private:
+  std::vector<BlockLabel> m_block_labels;
+};
+
+struct BlockLabelStackGuard {
+  explicit BlockLabelStackGuard(BlockLabelStack &stack, BlockLabel label)
+      : m_stack(stack) {
+    m_stack.push_scope(label);
+  }
+  ~BlockLabelStackGuard() { m_stack.pop_scope(); }
+
+  BlockLabelStackGuard(const BlockLabelStackGuard &) = delete;
+  BlockLabelStackGuard &operator=(const BlockLabelStackGuard &) = delete;
+  BlockLabelStackGuard(BlockLabelStackGuard &&) = delete;
+  BlockLabelStackGuard &operator=(BlockLabelStackGuard &&) = delete;
+
+private:
+  BlockLabelStack &m_stack;
+};
 //****************************************************************************
 } // namespace bust::codegen
 //****************************************************************************
